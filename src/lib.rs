f7264e7a65360380c23fa0d05c2a05f43a73b656
beto-bit <76757367+beto-bit@users.noreply.github.com>
2024-10-21 12:46:30 +0000

    Better way of continue generating primes

--- src/lib.rs
+++ src/lib.rs
@@ -4,52 +4,55 @@ use std::fs::File;
 use std::fs::OpenOptions;
 use std::path::Path;
 
-use std::io::{Seek, SeekFrom};
 use std::io::{BufReader, BufWriter, Read, Write};
+use std::io::{Seek, SeekFrom};
 
 use integer_encoding::FixedInt;
 use primality_test::IsPrime;
 
 pub fn generate_and_store_primes(path: impl AsRef<Path>, top: u32) -> Result<(), &'static str> {
     let file = OpenOptions::new()
-        .append(true)
+        .write(true)
         .create(true)
+        .truncate(false)
         .open(path)
         .map_err(|_| "Error opening file")?;
 
     let mut file = BufWriter::new(file);
 
-    for prime in (2u32..).filter(IsPrime::is_prime) {
+    for prime in (2u32..=top).filter(IsPrime::is_prime) {
         file.write(&prime.encode_fixed_light())
             .map_err(|_| "Error writing to file")?;
-
-        if prime > top {
-            break;
-        }
     }
 
     Ok(())
 }
 
 pub fn generate_and_append_primes(path: impl AsRef<Path>, top: u32) -> Result<(), &'static str> {
-    let file = OpenOptions::new()
+    let mut file = OpenOptions::new()
         .read(true)
         .append(true)
         .open(path)
         .map_err(|_| "Error opening file")?;
 
-    let mut br = BufReader::new(file);
-
     // Advance near the end
-    br.seek(SeekFrom::End(-4)).map_err(|_| "Error seeking near the EOF")?;
+    file.seek(SeekFrom::End(-4))
+        .map_err(|_| "Error seaking near the EOF")?;
 
-    // Read the bytes
+    // Read the bytes. It is unbuffered but it doesn't matter since we're doing it only once.
     let mut last_prime_bytes = [0u8; 4];
-    br.read(&mut last_prime_bytes).map_err(|_| "Error reading bytes")?;
+    file.read(&mut last_prime_bytes)
+        .map_err(|_| "Error reading bytes")?;
 
     let last_prime_found = u32::decode_fixed(&last_prime_bytes).ok_or("Error decoding bytes")?;
 
-    println!("{last_prime_found}");
+    // Store the next primes
+    let mut bw = BufWriter::new(file);
+
+    for prime in (last_prime_found + 1..=top).filter(IsPrime::is_prime) {
+        bw.write(&prime.encode_fixed_light())
+            .map_err(|_| "Error writing to file")?;
+    }
 
     Ok(())
 }
